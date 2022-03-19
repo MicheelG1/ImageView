78161d90cf0b24df1fe903074c086d580d92eefc
MicheelG1 <micheelgarcia870@gmail.com>
2022-03-18 20:43:45 -0600

    Initial commit

---
+++ app/src/main/java/edu/ieu/imageview/Main2Activity.java
@@ -0,0 +1,14 @@
+package edu.ieu.imageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
